9cff75ed250aec3ebd24cfcc58b54fe59f310907
Anthony <github email address>
2018-02-27 13:23:19 -0500

    coding users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,36 @@ class UsersController < ApplicationController
       erb :'users/signup'
     end
   end
+
+  post '/signup do'
+    if params[:username] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(:username => params[:username], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect '/moments'
+    end
+  end
+
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/moments'
+    else
+      redirect '/login'
+    end
+  end
+
+
+  get '/logout' do
+    if session[:user_id] != nil
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
 end
